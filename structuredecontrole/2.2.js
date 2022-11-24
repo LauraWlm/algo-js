944e9ab7ed3c45403e17bb43b95be2522012f7c5
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-24 14:00:08 +0100

    all

---
+++ structuredecontrole/2.2.js
@@ -0,0 +1,16 @@
+let min = prompt("Please enter a minimum number");
+let max = prompt("Pease enter a maximum number");
+let current = prompt ("Pease enter an other number");
+
+
+if ((current > min) && (current < max)){
+  console.log(current);
+} 
+
+else if (min > max) {
+    console.log("There might be a problem !");
+}
+
+else{
+    console.log("There might be a problem !");
+}
