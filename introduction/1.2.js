c1fe1266e331b59189a296781d3c101491f3ceea
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-24 10:36:01 +0100

    1.2.js

---
+++ introduction/1.2.js
@@ -0,0 +1,6 @@
+//Définissez trois variables : nom, prénom et ville. Affichez-les comme ceci : "Votre nom est Gérard Lambert et vous vivez à Paris.". name firstName city
+let firstName = "Laura";
+let name = "Wlm";
+let city = "Charleroi";
+console.log("Mon nom est" + firstName + name);
+console.log("et je vis à" + city);
