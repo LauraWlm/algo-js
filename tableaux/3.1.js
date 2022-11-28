141aef99045ed9cc7e29d313a144a9ebbbfb707a
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-28 14:37:04 +0100

    all

--- tableaux/3.1.js
+++ tableaux/3.1.js
@@ -1,12 +1,10 @@
-const array1 = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5];
 
-const initialValue = 0;
-const sumWithInitial = array1.reduce(
-  (accumulator, currentValue) => accumulator + currentValue,
-  initialValue
-);
+console.log(arr[0] + arr[1] + arr[2] + arr[3] + arr[4] );
 
-console.log(sumWithInitial);
+let numbersArray = [100, 101, 102];
+
+console.log(numbersArray[0] + numbersArray[1] + numbersArray[2]);
 
 
 
