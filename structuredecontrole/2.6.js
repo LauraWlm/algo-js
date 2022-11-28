f3616f1ddecf506506bd4112d93b1c054c5c6c64
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-28 10:56:54 +0100

    2.6

---
+++ structuredecontrole/2.6.js
@@ -0,0 +1,8 @@
+let number = prompt("Choose a number between 1 and 7");
+
+let days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday", "Sunday");
+
+while (number < 7) {
+    console.log(days);
+    break;
+}
