4050f6e1be247df51f34b14e4f394549bce74c09
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-28 14:05:34 +0100

    all

---
+++ tableaux/3.0.js
@@ -0,0 +1,12 @@
+/*let arr = ["Apple", "Pear", "Banana"];
+
+for (let i = 0; i < arr.length; i += 1) {
+  console.log("Do you want to eat a " + arr[i] + "?");
+}
+
+let arr = ["Apple", "Pear", "Banana"];
+
+for (let elem of arr) {
+  console.log("Do you want to eat a " + elem + "?");
+}*/
+
