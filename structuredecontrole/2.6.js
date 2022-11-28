3592ca808d1f50dcdd98e5e8e7b686b9e5f6835b
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-28 11:36:11 +0100

    all

--- structuredecontrole/2.6.js
+++ structuredecontrole/2.6.js
@@ -1,8 +1,22 @@
-let number = prompt("Choose a number between 1 and 7");
-
-let days = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday", "Sunday");
-
-while (number < 7) {
-    console.log(days);
-    break;
+let number =prompt( "donne un chiffre entre 1 et 7");
+if (number ==1){
+    alert ('lundi');
+}
+if (number ==2){
+    alert ('mardi');
+}
+if (number ==3){
+    alert ('mercredi');
+}
+if (number =4){
+    alert ('jeudi');
+}
+if (number ==5){
+    alert ('vendredi');
+}
+if (number ==6){
+    alert ('samedi');
+}
+if (number ==7){
+    alert ('dimanche');
 }
