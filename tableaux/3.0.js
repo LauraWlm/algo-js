f68397d874d4f7b886f18aeb13ad90b1f35448e7
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-28 14:11:53 +0100

    all

--- tableaux/3.0.js
+++ tableaux/3.0.js
@@ -10,3 +10,8 @@ for (let elem of arr) {
   console.log("Do you want to eat a " + elem + "?");
 }*/
 
+let arr = ["Arno Volts","Aurélien Mariaule","Aurore Lemaire","Benjamin Porsont","Céline Verreydt","Corentin Miserque","Dominique Coppée","Edouard de Romrée de Vichenet","Hugo Goorickx","Jonathan Manes","jonathan bajoux","Laura Wilhelmi","Lysie Soyez","Marnie Benalia","Mathilde Cornelis","Milo Bonnet","Nadim El Nakadi","Nathalie Vanden Abeele","Nathalie Goffette","Nour Everaert","Pierre Mauriello","Quentin Bource ","Virginie Dourson"];
+
+for (let elem of arr) {
+  console.log("Hello " + elem + " how are you?");
+}
